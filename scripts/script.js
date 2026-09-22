8716507da8af21c9e50a1dd77545f25a67595d4b
StenioEric <stenioeric1@gmail.com>
2026-09-22 16:16:57 -0300

    implementação para não repetição de números

--- scripts/script.js
+++ scripts/script.js
@@ -2,36 +2,43 @@ const quantidade = document.getElementById("quantidade")
 const min = document.getElementById("minimo")
 const max = document.getElementById("maximo")
 const sort = document.getElementById("sortear")
+const checkbox = document.querySelector(".switch input")
 
-sort.addEventListener("click" , (event) => {
+sort.addEventListener("click", (event) => {
     event.preventDefault();
 
-    let minValeu = min.value 
+    let minValeu = min.value
     let maxValue = max.value
     let quantValue = quantidade.value
-    
+
     const numbers = []
-    
+
     let i = 0
     while (i < quantValue) {
         let result = sortRandom(minValeu, maxValue)
-        console.log(result)
+
+        if (checkbox.checked) {
+
+            while (number = numbers.includes(result)) {
+                result = sortRandom(minValeu, maxValue)
+            }
+
+            numbers.push(result)
+
+        } else {
+            numbers.push(result)
+        }
         i++
-        numbers.push(result)
     }
-    
+
     console.log(numbers)
-
-
-    
-    
 
 })
 
 function sortRandom(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
